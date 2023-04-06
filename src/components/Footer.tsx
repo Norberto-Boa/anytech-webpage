4c7722892499dbe552184c51b5ba0b05ede4dbe7
Norberto Boa <nboa.26@gmail.com>
2023-04-07 01:06:46 +0200

    Launch

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="w-full bg-zinc-200 py-2">
-      <p className="max-w-5xl mx-auto text-center">Desenvolvido pelo <span className="text-sky-500 font-bold">&copy;Rei da Matola</span></p>
+      <p className="max-w-4xl mx-auto text-center">Desenvolvido pelo <span className="text-sky-500 font-bold">&copy;Rei da Matola</span></p>
     </div>
   )
 }
